c317b4fdd0ea965bdb7324ca7cb1af2839abe48f
Saurabh Kalambe <saurabhkalambe13.com>
2026-01-16 17:39:07 +0530

    subStringWithoutRepeatingCharacters

---
+++ arrays/subStringWithoutRepeatingCharacter.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// time complexity O(n)  Space complexity O(min(m,n))
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let left = 0; maxLen =0;
+
+    for(let right = 0 ; right <s.length; right++){
+        while(set.has(s[right])){
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLen =  Math.max(maxLen , right -left +1);
+    }
+    return maxLen;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb")) // 3
